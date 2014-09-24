1b1c4dd7c0174c4b893f55dd920aaf55988c2136
Helen Lau <hlau@lyft.com>
2014-09-24 08:38:44 -0700

    chap33-locationTracking

---
+++ RunTracker/app/src/main/java/com/example/helenlau/runtracker/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.example.helenlau.runtracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by helenlau on 9/16/14.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //always return activity_fragment
+        //setContentView(R.layout.activity_fragment);
+        //dynamic fragment
+        setContentView(getLayoutResId());
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
+    }
+
+}
